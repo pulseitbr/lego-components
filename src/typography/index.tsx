69568dfc36c1317281aa2d4639d68457598df4af
Garcez <allan.f.garcez@gmail.com>
2019-09-28 17:07:37 -0300

    Typography components

---
+++ src/typography/index.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import Theme from "../styles";
+export const Title = styled.div`
+  font-weight: bold;
+  font-size: 2.25rem;
+  color: ${Theme.darkLight};
+  font-family: "Comfortaa", Arial, Helvetica, sans-serif;
+`;
+
+export const SubTitle = styled.div`
+  font-weight: bold;
+  font-size: 1.65rem;
+  color: ${Theme.darkLight};
+  font-family: "Comfortaa", Arial, Helvetica, sans-serif;
+`;
