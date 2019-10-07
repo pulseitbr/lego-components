1d7af12f26b1a5291ae8bb817a53cb2af559634e
Garcez <allan.f.garcez@gmail.com>
2019-10-07 11:39:20 -0300

    AddressBox component

--- src/tenants/AddressBox.tsx
+++ src/tenants/AddressBox.tsx
@@ -4,7 +4,7 @@ import FlatList from "../list/FlatList";
 import { SubTitle } from "../typography";
 import { formatCep } from "sidekicker/lib/strings/Format";
 import Radiobox from "../form/Radiobox";
-import { MdHome } from "react-icons/md";
+import { MdHome, MdStore } from "react-icons/md";
 import { transparentize } from "polished";
 import Theme from "../styles";
 import { isEmpty } from "sidekicker/lib/comparable";
@@ -18,37 +18,67 @@ type Endereco = {
     bairro: string;
     cidade: string;
     uf: string;
-    tipoEndereco: {
-        codigo: number;
-        descricao: "";
-    };
-    tipoLogradouro: {
-        id: number;
-        descricao: "";
-    };
 };
 
 type BindProps = {
-    onChange(nrSeqEndereco: number): any;
+    isHome?: boolean;
+    inlineLayout?: boolean;
     idSelectAddress: number | null;
+    onChange(nrSeqEndereco: number | null): any;
 };
 
 type Props = {
+    listGetter: () => any;
     dataSource: Endereco[];
+    emptyHomeMessage: React.ReactNode;
+    emptyStoreMessage: React.ReactNode;
 } & Omit<BindProps, "idSelectAddress">;
 
 const transparentBackground = transparentize(0.4, Theme.primaryAlpha);
 
+const getIcon = (isHome: boolean) => (isHome ? <MdHome /> : <MdStore />);
+
 const BindCard = (props: BindProps) => (endereco: Endereco) => {
     const isSelected = props.idSelectAddress === endereco.nrSeqEndereco;
 
-    const onChange = () => {
-        props.onChange(endereco.nrSeqEndereco);
-    };
+    const onChange = () => props.onChange(endereco.nrSeqEndereco);
+
+    if (!!props.inlineLayout) {
+        return (
+            <View
+                span="50%"
+                role="button"
+                onClick={onChange}
+                style={{ padding: "0.15rem", textAlign: "left", cursor: "pointer" }}
+            >
+                <Container
+                    style={{
+                        padding: "0.1rem",
+                        border: `1px solid ${Theme.darkAlpha}`,
+                        backgroundColor: isSelected ? transparentBackground : "transparent"
+                    }}
+                >
+                    <Left style={{ flex: "0 0 70%" }}>
+                        <SubTitle style={{ fontSize: "1.25rem" }}>
+                            {getIcon(!!props.isHome)} {formatCep(endereco.cep)}
+                        </SubTitle>
+                    </Left>
+                    <Right style={{ flex: "0 0 30%", textAlign: "right", justifyContent: "flex-end" }}>
+                        <Radiobox name={`check-address-${endereco.nrSeqEndereco}`} value={isSelected} />
+                    </Right>
+                    <View span="100%">
+                        {endereco.logradouro}, {endereco.numero}
+                        {!!endereco.complemento ? `, ${endereco.complemento}` : ""}. {endereco.bairro} -{" "}
+                        {endereco.cidade} - {endereco.uf}
+                    </View>
+                </Container>
+            </View>
+        );
+    }
 
     return (
         <View
-            span="20%"
+            span="33%"
             medium="33%"
             role="button"
             onClick={onChange}
@@ -63,8 +93,8 @@ const BindCard = (props: BindProps) => (endereco: Endereco) => {
             >
                 <Container>
                     <Left style={{ flex: "0 0 70%" }}>
-                        <SubTitle>
-                            <MdHome /> {formatCep(endereco.cep)}
+                        <SubTitle style={{ fontSize: "1.25rem" }}>
+                            {getIcon(!!props.isHome)} {formatCep(endereco.cep)}
                         </SubTitle>
                     </Left>
                     <Right style={{ flex: "0 0 30%", textAlign: "right", justifyContent: "flex-end" }}>
@@ -91,14 +121,33 @@ const BindCard = (props: BindProps) => (endereco: Endereco) => {
     );
 };
 
-const AddressCard = ({ dataSource, onChange }: Props) => {
+const AddressBox = ({
+    dataSource,
+    emptyHomeMessage,
+    emptyStoreMessage,
+    listGetter,
+    onChange,
+    inlineLayout = false,
+    isHome = true
+}: Props) => {
     const [idSelectAddress, setIdSelectAddress] = useState(null as number | null);
+
+    const useInlineLayout = inlineLayout || dataSource.length > 5;
+
+    const emptyMessage = isHome ? emptyHomeMessage : emptyStoreMessage;
+
+    useEffect(() => {
+        listGetter();
+    }, []);
 
     useEffect(() => {
         if (!isEmpty(dataSource)) {
             const id = dataSource[0].nrSeqEndereco!;
             setIdSelectAddress(id);
             onChange(id);
+        } else {
+            setIdSelectAddress(null);
+            onChange(null);
         }
     }, [dataSource.length]);
 
@@ -111,12 +160,12 @@ const AddressCard = ({ dataSource, onChange }: Props) => {
         <Container>
             <FlatList
                 data={dataSource}
-                emptyComponent="Não possui endereços"
+                emptyComponent={emptyMessage}
                 key={`${dataSource.length}-flat-list-address`}
-                component={BindCard({ onChange: changeSelect, idSelectAddress })}
+                component={BindCard({ onChange: changeSelect, idSelectAddress, inlineLayout: useInlineLayout, isHome })}
             />
         </Container>
     );
 };
 
-export default AddressCard;
+export default AddressBox;
