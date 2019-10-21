d0fc36036a1cd3a1dff1c80c9da67852f8b831f5
Garcez <allan.f.garcez@gmail.com>
2019-10-21 09:40:10 -0300

    Add Text component

--- src/typography/index.tsx
+++ src/typography/index.tsx
@@ -1,15 +1,27 @@
 import styled from "styled-components";
 import Theme from "../styles";
-export const Title = styled.div`
-  font-weight: bold;
-  font-size: 2.25rem;
-  color: ${Theme.darkLight};
-  font-family: "Comfortaa", Arial, Helvetica, sans-serif;
+export const Title = styled.h1`
+    font-weight: bold;
+    font-size: 2.25rem;
+    color: ${Theme.darkLight};
+    font-family: "Comfortaa", Arial, Helvetica, sans-serif;
 `;
 
-export const SubTitle = styled.div`
-  font-weight: bold;
-  font-size: 1.65rem;
-  color: ${Theme.darkLight};
-  font-family: "Comfortaa", Arial, Helvetica, sans-serif;
+export const SubTitle = styled.h2`
+    font-weight: bold;
+    font-size: 1.65rem;
+    color: ${Theme.darkLight};
+    font-family: "Comfortaa", Arial, Helvetica, sans-serif;
+`;
+type TextType = {
+    paragraph: boolean | string;
+};
+export const Text = styled.p.attrs(({ paragraph, ...props }: TextType) => {
+    const size = paragraph === true ? "1rem" : typeof paragraph === "string" ? paragraph : "0";
+    return { ...props, paragraph: size };
+})`
+    text-anchor: bottom;
+    line-height: 1.5rem;
+    text-shadow: 0.5px 0.5px #20202010;
+    text-indent: ${(props) => props.paragraph};
 `;
