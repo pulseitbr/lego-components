05d131a8655a81e0e35d631a30a75590378373ba
Garcez <allan.f.garcez@gmail.com>
2019-10-21 09:39:32 -0300

    Console abstract

---
+++ src/utils/Console.ts
@@ -0,0 +1,12 @@
+export default {
+    array: console.table,
+    table: console.table,
+    style: (text: string, color: string, fontSize: string) => console.log(new Date(), `%c${text}`, `color: ${color};font-size: ${fontSize}px;`),
+    group: (label: string, ...data: any[]) => {
+        console.group(label);
+        data.forEach((x) => {
+            console.info(new Date(), x);
+        });
+        console.groupEnd();
+    }
+};
